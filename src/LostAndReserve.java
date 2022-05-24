d8d26f6553255019a58e45186b53d9116bdce2a9
JackGhL <xxt1205@gmail.com>
2022-05-24 15:28:58 +0900

    Update LostAndReserve.java

--- src/LostAndReserve.java
+++ src/LostAndReserve.java
@@ -1,26 +1,42 @@
+import java.sql.Array;
 
 public class LostAndReserve {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution5 s = new Solution5();
+		
 		int[] lost = {2,4};
-		int[] reserver = {1,3,5};
-		s.solution(5, lost, reserver);
+		int[] reserve = {3};
+		
+		System.out.println(s.solution(5, lost, reserve));
 	}
 
 }
+
 class Solution5 {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        for(int i = 0; n >= i; i++) {
-        	if(lost[i] - reserve[i] != 1 || lost[i] - reserve[i] != -1) {
-        		n = n -1;
-        	}else if(reserve[i] == null) {
-        		
-        	}
-        }
-        System.out.println(n);
-        return answer;
-    }
+	public int solution(int n, int[] lost, int[] reserve) {
+		int answer = 0;
+		int result = n - lost.length;
+		int[] reserve1 = ArrayUtils
+		
+		System.out.println(lost.length);
+		System.out.println(result);
+		int i = 0;
+		for (int j = 0; lost.length - 1 >= j; j++) {
+			if(reserve[i] == -100 || lost[j] == -100) {
+				break;
+			}
+			if(lost[j] - reserve[i] == 1 || lost[j] - reserve[i] == -1) {
+				result = result + 1;
+				i += 1;
+			}
+		}
+			
+				
+				
+		answer = result;
+		return answer;
+	
+	}
 }
