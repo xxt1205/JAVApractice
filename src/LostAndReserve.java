6e7ca3e1c55509689dc43d1fd649527574314467
JackGhL <xxt1205@gmail.com>
2022-05-25 23:41:08 +0900

    수정2차

--- src/LostAndReserve.java
+++ src/LostAndReserve.java
@@ -1,4 +1,4 @@
-import java.sql.Array;
+import java.util.ArrayList;
 
 public class LostAndReserve {
 
@@ -6,10 +6,10 @@ public class LostAndReserve {
 		// TODO Auto-generated method stub
 		Solution5 s = new Solution5();
 		
-		int[] lost = {2,4};
-		int[] reserve = {3};
+		int[] lost = {3};
+		int[] reserve = {1};
 		
-		System.out.println(s.solution(5, lost, reserve));
+		System.out.println(s.solution(3, lost, reserve));
 	}
 
 }
@@ -18,21 +18,31 @@ class Solution5 {
 	public int solution(int n, int[] lost, int[] reserve) {
 		int answer = 0;
 		int result = n - lost.length;
-		int[] reserve1 = ArrayUtils
 		
-		System.out.println(lost.length);
-		System.out.println(result);
+		ArrayList<Integer> lost1 = new ArrayList<>(lost.length);
+		for(int i=0; lost.length - 1 >= i; i++) {
+			lost1.add(lost[i]);
+		}
+		lost1.add(-5);
+		
+		ArrayList<Integer> reserve1 = new ArrayList<>(reserve.length);
+		for(int i=0; reserve.length - 1 >= i; i++) {
+			reserve1.add(reserve[i]);
+		}
+		reserve1.add(-5);
+		
+		System.out.println(lost1);
+		System.out.println(reserve1);
 		int i = 0;
-		for (int j = 0; lost.length - 1 >= j; j++) {
-			if(reserve[i] == -100 || lost[j] == -100) {
-				break;
-			}
-			if(lost[j] - reserve[i] == 1 || lost[j] - reserve[i] == -1) {
-				result = result + 1;
-				i += 1;
+		for(int j = 0; lost1.size() - 1 >= j; j++) {
+			if(lost1.get(j) == -5) break;
+			if(reserve1.get(i) == -5)break;
+			if(lost1.get(j) - reserve1.get(i) == 1 ||lost1.get(j) - reserve1.get(i) == -1) {
+				result += 1;
+				i++;
 			}
 		}
-			
+			System.out.println("return : " + result);
 				
 				
 		answer = result;
