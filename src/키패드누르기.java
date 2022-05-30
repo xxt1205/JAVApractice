5d19702d220eb14589a7bc345ae444e48bfd522d
JackGhL <xxt1205@gmail.com>
2022-05-30 16:32:38 +0900

    키패드누르기

---
+++ src/키패드누르기.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 키패드누르기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+		String hand = "right";
+		Solution14 s = new Solution14();
+		s.solution(numbers, hand);
+	}
+
+}
+
+class Solution14 {
+    public String solution(int[] numbers, String hand) {
+    	
+    	String answer = "";
+    	
+    	String[] str = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+    	ArrayList<Integer> moveL = new ArrayList<Integer>();
+    	ArrayList<Integer> moveR = new ArrayList<Integer>();
+    	int [] middle = {2, 5, 8, 0};
+    	
+    	for(int i = 0; numbers.length > i; i++) {
+    		int num = numbers[i];
+    		if() {
+    			
+    		}
+    		switch(numbers[i]) {
+    		case 1 : 
+    		case 4 :
+    		case 7 : answer.concat("L"); moveL.add(numbers[i]);
+    		case 3 :
+    		case 6 :
+    		case 9 : answer.concat("R"); moveR.add(numbers[i]);	
+    		}
+    	}
+        
+        return answer;
+    }
+}
